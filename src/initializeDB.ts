8374f6dc9c776ff6f6ad7b0ba0cd2c455806a25c
Ashubh01 <shubhamdhokare01@gmail.com>
2025-01-20 21:11:49 +0530

    Added a dummy data creation function at build time

--- src/initializeDB.ts
+++ src/initializeDB.ts
@@ -1,2 +1,21 @@
 import { Question } from "./schemas";
+import mongoose from "mongoose";
 
+async function addQuestions () {                            // ADD SOME DUMMY QUESTION ON BUILD TIME
+    const dummyQuestions = [
+        { text: "Question 1", options: ["A", "B", "C"], correctOption: 2},
+        { text: "Question 2", options: ["A", "B", "C", "D", "E"], correctOption: 1},
+        { text: "Question 3", options: ["A", "B", "C", "D"], correctOption: 4}
+    ];
+
+    try {
+        await mongoose.connect(process.env.DATABASE_URL as string);
+        await Question.insertMany(dummyQuestions);
+        await mongoose.disconnect();
+    } catch (err) {
+        console.error(err)
+        process.exit(1);
+    }
+}
+
+addQuestions();
